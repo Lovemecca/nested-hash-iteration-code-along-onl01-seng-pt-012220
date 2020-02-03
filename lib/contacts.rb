d765d93bb07e97ba42020f2b2351928ac1d80511
Mecca Jay <mecca.nicolej@gmail.com>
2020-02-03 21:23:28 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -10,6 +10,15 @@ require 'pry'
   end
 end	
   
-def remove_strawberry(contacts)
 
-end
+def remove_strawberry(contacts)
+  freddy = contacts["Freddy Mercury"]
+
+  freddy.each do |attribute, value|
+    if attribute == :favorite_icecream_flavors
+      value.delete_if{ |ice_cream| ice_cream == "strawberry" }
+    end
+  end
+  contacts
+end 
+
